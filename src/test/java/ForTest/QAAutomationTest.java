09235fe64af244da83eaf4ac6e143a3a8a71e555
Anastasia Astakhova <anastasia@MacBook-Pro-Anastasia.local>
2017-11-24 11:38:44 +0100

    Initial commit of QA Automation test for Actum company

---
+++ src/test/java/ForTest/QAAutomationTest.java
@@ -0,0 +1,111 @@
+package ForTest;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class QAAutomationTest {
+    private static FirefoxDriver driver;
+    WebElement element;
+
+    @BeforeClass
+    public static void openBrowser(){
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void passTheTest(){
+
+        System.out.println("Starting test " + new Object(){}.getClass().getEnclosingMethod().getName());
+        driver.get("https://docs.google.com/forms/d/e/1FAIpQLSeY_W-zSf2_JxR4drhbgIxdEwdbUbE4wXhxHZLgxZGiMcNl7g/viewform?c=0&w=1");
+
+        // Filling first step questions
+        List<WebElement> itemList = driver.findElements(By.xpath("/.//*[text()='Check this']/../../../div[@role='checkbox']"));
+        for (int i=0; i<itemList.size();i++)
+            itemList.get(i).click();
+
+        LocalDate dt = LocalDate.now().plusDays(6);
+        String day = new String(dt.format(DateTimeFormatter.ofPattern("dd")));
+        String month = new String(dt.format(DateTimeFormatter.ofPattern("MM")));
+        String year = new String(dt.format(DateTimeFormatter.ofPattern("YYYY")));
+        WebElement date = driver.findElement(By.xpath("//div[@class='freebirdFormviewerViewItemsDateDateInputs']"));
+        List<WebElement> dateList = date.findElements(By.xpath("//input[@class='quantumWizTextinputPaperinputInput exportInput']"));
+        dateList.get(0).sendKeys(day);
+        dateList.get(1).sendKeys(month);
+        dateList.get(2).sendKeys(year);
+
+        String nextButtonFirstStep = "//div[@class='freebirdFormviewerViewNavigationButtons']/div";
+        driver.findElement(By.xpath(nextButtonFirstStep)).click();
+        try {
+            driver.findElement(By.xpath("//div[contains(@class, 'HasError')]"));
+        } catch (Exception e){
+            System.out.println("Error message wasn't found.");
+            Assert.assertNull(e.getMessage());
+        }
+
+        String elCurrMonth = "//div[@class='freebirdFormviewerViewItemsTextItemWrapper']/.//input";
+        driver.findElement(By.xpath(elCurrMonth)).sendKeys(dt.getMonth().toString());
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        try {
+            TimeUnit.SECONDS.sleep(1); //to add adequate timeout
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.findElement(By.xpath(nextButtonFirstStep)).click();
+
+        // Filling second step questions
+        String movies = "House M.D., Friends, Now you see me";
+        driver.findElement(By.xpath("//textarea[@class='quantumWizTextinputPapertextareaInput exportTextarea']")).sendKeys(movies);
+
+        driver.findElement(By.xpath("//div[@class='quantumWizMenuPaperselectOptionList']")).click();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Actions actions = new Actions(driver);// to finish
+        actions.moveToElement(driver.findElement(By.xpath("//div[@data-value='Black']"))).perform();
+        driver.findElement(By.xpath("//div[@data-value='Black']/..")).click();
+
+        driver.findElement(By.xpath("//div[@class='freebirdFormviewerViewNavigationButtons']/div[1]")).click();
+
+        // Returned to first step section
+        WebElement currMonth = driver.findElement(By.xpath(elCurrMonth));
+        String oldValue = currMonth.getAttribute("value");
+        currMonth.clear();
+        driver.findElement(By.xpath(elCurrMonth)).sendKeys(new StringBuilder(oldValue).reverse().toString());
+        driver.findElement(By.xpath(nextButtonFirstStep)).click();
+
+        //Assertion of second step section
+        String actualMovies = driver.findElement(By.xpath("//textarea[@class='quantumWizTextinputPapertextareaInput exportTextarea']"))
+                .getAttribute("value");
+        Assert.assertTrue("Actual and expected strings are not equal.", actualMovies.equals(movies));
+
+        String actualColor = driver.findElement(By.xpath("//div[@class='quantumWizMenuPaperselectOptionList']/./div[@aria-selected='true']"))
+                .getAttribute("data-value");
+        Assert.assertTrue("Actual and expected strings are not equal.", actualColor.equals("Grey"));
+        driver.findElement(By.xpath("//div[@class='freebirdFormviewerViewNavigationButtons']/div[2]")).click();
+
+        //Third Step section
+        driver.findElement(By.xpath("//div[@aria-label='Yes']")).click();
+        driver.findElement(By.xpath("//div[@class='freebirdFormviewerViewNavigationButtons']/div[2]")).click();
+        System.out.println("Ending test " + new Object(){}.getClass().getEnclosingMethod().getName());
+    }
+
+    @AfterClass
+    public static void closeBrowser(){
+        driver.quit();
+    }
+}
